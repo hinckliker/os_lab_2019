e9f3bd6ad01fa77e01a7153ae4c2c6d57494ca84
sporist <>
2021-04-20 13:11:33 +0000

    nothing

---
+++ lab1/src/script.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+date -u
+echo $PATH
+pwd
--- lab3/src/parallel_min_max.c
+++ lab3/src/parallel_min_max.c
@@ -15,43 +15,48 @@
 #include "find_min_max.h"
 #include "utils.h"
 
+
 int main(int argc, char **argv) {
-  int seed = -1;
+  int seed = -1; // Число на основе которого генерируется массив 
   int array_size = -1;
   int pnum = -1;
   bool with_files = false;
-
-  while (true) {
+  while (true) 
+  {
     int current_optind = optind ? optind : 1;
-
     static struct option options[] = {{"seed", required_argument, 0, 0},
                                       {"array_size", required_argument, 0, 0},
                                       {"pnum", required_argument, 0, 0},
                                       {"by_files", no_argument, 0, 'f'},
                                       {0, 0, 0, 0}};
-
+    
     int option_index = 0;
+    // -f - опция
     int c = getopt_long(argc, argv, "f", options, &option_index);
-
     if (c == -1) break;
-
     switch (c) {
       case 0:
         switch (option_index) {
           case 0:
             seed = atoi(optarg);
-            // your code here
-            // error handling
+            if(optarg<=0)
+            {
+                return 1;
+            }
             break;
           case 1:
             array_size = atoi(optarg);
-            // your code here
-            // error handling
+            if(optarg<=0)
+            {
+                return 1;
+            }
             break;
           case 2:
             pnum = atoi(optarg);
-            // your code here
-            // error handling
+            if(optarg<=0)
+            {
+                return 1;
+            }
             break;
           case 3:
             with_files = true;
@@ -72,44 +77,77 @@ int main(int argc, char **argv) {
         printf("getopt returned character code 0%o?\n", c);
     }
   }
-
-  if (optind < argc) {
+  //Проверка на количество введеных аргументов которые не являются опцией
+  if (optind < argc)
+  {
     printf("Has at least one no option argument\n");
     return 1;
   }
-
-  if (seed == -1 || array_size == -1 || pnum == -1) {
+  // Проверка количества введенных аргументов 
+  if (seed == -1 || array_size == -1 || pnum == -1) 
+  {
     printf("Usage: %s --seed \"num\" --array_size \"num\" --pnum \"num\" \n",
            argv[0]);
-    return 1;
+    return 1; 
   }
 
   int *array = malloc(sizeof(int) * array_size);
   GenerateArray(array, array_size, seed);
   int active_child_processes = 0;
 
+
   struct timeval start_time;
   gettimeofday(&start_time, NULL);
-
+  int n = array_size / pnum; 
+  int pipefd[2];
+  pipe(pipefd); // создает pipe, однонаправленный канал данных, который можно использовать для межпроцессного взаимодействия.
+  /* Данные, записанные в конец канала для записи, 
+  буферизуются ядром до тех пор, пока не будут прочитаны из 
+  конца канала для чтения  */
   for (int i = 0; i < pnum; i++) {
-    pid_t child_pid = fork();
+    /*Процессы создаются системным вызовом fork (так что операция создания нового процесса иногда вызывает раздваивание процесса).
+     Дочерний процесс, созданный fork - точный аналог первоначального родительского процесса, 
+     за исключением того, что он имеет собственный ID. */
+    pid_t child_pid = fork(); // Pid_t тип данных для ID процесса.
+    /*Если операция является успешной, то и родительский и дочерний процессы видят что fork возвращается,
+    но с различными значениями: она возвращает значение 0 в дочернем процессе
+    и ID порожденного процесса (ребенка) в родительском процессе.
+    Если создание процесса потерпело неудачу, fork возвращает значение -1 в родительском процессе. */
     if (child_pid >= 0) {
-      // successful fork
       active_child_processes += 1;
       if (child_pid == 0) {
         // child process
-
+        // Получили min_max
+        struct MinMax min_max;
+        min_max = GetMinMax(array, (unsigned int)i * n, (unsigned int)(i + 1) * n);
         // parallel somehow
-
-        if (with_files) {
-          // use files here
-        } else {
+        // Запись в файл
+        if (with_files)
+        {
+            FILE *fp;
+            if (i == 0)
+            {
+            fp = fopen("output.txt", "w"); // записывает в первом цикле 
+            }
+            else
+            {
+            fp = fopen("output.txt", "a"); // добавляет в старый тхт
+            }
+          fprintf(fp, "%d %d\n", min_max.min, min_max.max);
+          fclose(fp);
+        } 
+        else 
+        {
+            //0 - чтение 1 - запись
+            // запись в пайп
+            write(pipefd[1], &min_max, sizeof(struct MinMax));
           // use pipe here
         }
         return 0;
       }
-
-    } else {
+    } 
+    else 
+    {
       printf("Fork failed!\n");
       return 1;
     }
@@ -117,7 +155,7 @@ int main(int argc, char **argv) {
 
   while (active_child_processes > 0) {
     // your code here
-
+    wait(NULL); // Ждем пока все доработают
     active_child_processes -= 1;
   }
 
@@ -130,8 +168,28 @@ int main(int argc, char **argv) {
     int max = INT_MIN;
 
     if (with_files) {
-      // read from files
+        // из файла берем и сравниаваем
+      struct MinMax min_max;
+      FILE *fp;
+      int j;
+      fp = fopen("output.txt", "r");
+      for (j = 0; j < i || (i == 0 && j <= i); j++)
+      {
+        fscanf(fp, "%d %d", &min_max.min, &min_max.max);
+        if (min < min_max.min)
+          min_max.min = min;
+        if (max > min_max.max)
+          min_max.max = max;
+      }
+      fclose(fp);
+      min = min_max.min;
+      max = min_max.max;
     } else {
+      struct MinMax min_max;
+      // Тупо читаем из pipe
+      read(pipefd[0], &min_max, sizeof(struct MinMax));
+      min = min_max.min;
+      max = min_max.max;
       // read from pipes
     }
 
@@ -144,9 +202,7 @@ int main(int argc, char **argv) {
 
   double elapsed_time = (finish_time.tv_sec - start_time.tv_sec) * 1000.0;
   elapsed_time += (finish_time.tv_usec - start_time.tv_usec) / 1000.0;
-
   free(array);
-
   printf("Min: %d\n", min_max.min);
   printf("Max: %d\n", min_max.max);
   printf("Elapsed time: %fms\n", elapsed_time);
